808b80350213af4ad82ab53298a839d682b53806
Rob Nelson <nexisentertainment@gmail.com>
2015-01-27 22:09:32 -0800

    Massive fucking cleanup
    Whoever did this originally needs to punch themselves in the balls holy shit
    
    # Changes:
     * Remove all of the dependencies since they're in NuGet.
     * Add packages/ and *.zip to .gitignore
     * Make a much simpler debug overlay
     * Adjustable camera angle code using basic trig
     * General code cleanup

--- MSpriteRenderer/Source/DebugOverlay.cs
+++ MSpriteRenderer/Source/DebugOverlay.cs
@@ -1,55 +1,77 @@
 ﻿using Mogre;
 
-namespace MSpriteRenderer {
+namespace MSpriteRenderer
+{
 
-  public class DebugOverlay {
+    public class DebugOverlay
+    {
+        TextManager mTextManager;
 
-    public DebugOverlay(RenderWindow window) {
-      mWindow = window;
+        Text mFPSAvg;
+        Text mFPSCurr;
+        Text mFPSBest;
+        Text mFPSWorst;
+        Text mTris;
+        Text mModes;
+        public DebugOverlay(RenderWindow window)
+        {
+            mWindow = window;
+            mTextManager = new TextManager();
 
-      var debugOverlay = OverlayManager.Singleton.GetByName("Core/DebugOverlay");
-      debugOverlay.Show();
+            mFPSAvg = new Text("Average FPS:") { Position = new System.Drawing.Point(10, 0) };
+            mFPSCurr = new Text("Current FPS:") { Position = new System.Drawing.Point(10, 15) };
+            mFPSBest = new Text("Best FPS:") { Position = new System.Drawing.Point(10, 30) };
+            mFPSWorst = new Text("Worst FPS:") { Position = new System.Drawing.Point(10, 45) };
+            mTris = new Text("Triangle Count:") { Position = new System.Drawing.Point(10, 60) };
+            mModes = new Text("???") { Position = new System.Drawing.Point(10, 75) };
 
-      mGuiAvg = OverlayManager.Singleton.GetOverlayElement("Core/AverageFps");
-      mGuiCurr = OverlayManager.Singleton.GetOverlayElement("Core/CurrFps");
-      mGuiBest = OverlayManager.Singleton.GetOverlayElement("Core/BestFps");
-      mGuiWorst = OverlayManager.Singleton.GetOverlayElement("Core/WorstFps");
-      mGuiTris = OverlayManager.Singleton.GetOverlayElement("Core/NumTris");
-      mModesText = OverlayManager.Singleton.GetOverlayElement("Core/NumBatches");
+            mTextManager.AddText(mFPSAvg);
+            mTextManager.AddText(mFPSCurr);
+            mTextManager.AddText(mFPSBest);
+            mTextManager.AddText(mFPSWorst);
+            mTextManager.AddText(mTris);
+            mTextManager.AddText(mModes);
+
+            mTextManager.ShowAllText();
+        }
+
+        public string AdditionalInfo
+        {
+            set { mAdditionalInfo = value; }
+            get { return mAdditionalInfo; }
+        }
+
+        public void Update(float timeFragment)
+        {
+            if (timeSinceLastDebugUpdate > 0.5f)
+            {
+                var stats = mWindow.GetStatistics();
+
+                mFPSAvg.Caption= "Average FPS: " + stats.AvgFPS;
+                mFPSCurr.Caption = "Current FPS: " + stats.LastFPS;
+                mFPSBest.Caption = "Best FPS: " + stats.BestFPS + " " + stats.BestFrameTime + " ms";
+                mFPSWorst.Caption = "Worst FPS: " + stats.WorstFPS + " " + stats.WorstFrameTime + " ms";
+                mTris.Caption = "Triangle Count: " + stats.TriangleCount;
+                mModes.Caption = mAdditionalInfo;
+
+                timeSinceLastDebugUpdate = 0;
+            }
+            else
+            {
+                timeSinceLastDebugUpdate += timeFragment;
+            }
+        }
+
+        protected RenderWindow mWindow;
+        protected float timeSinceLastDebugUpdate = 1;
+        protected OverlayElement mGuiAvg;
+        protected OverlayElement mGuiCurr;
+        protected OverlayElement mGuiBest;
+        protected OverlayElement mGuiWorst;
+        protected OverlayElement mGuiTris;
+        protected OverlayElement mModesText;
+        protected string mAdditionalInfo = "";
+
     }
-
-    public string AdditionalInfo {
-      set { mAdditionalInfo = value; }
-      get { return mAdditionalInfo; }
-    }
-
-    public void Update(float timeFragment) {
-      if(timeSinceLastDebugUpdate > 0.5f) {
-        var stats = mWindow.GetStatistics();
-
-        mGuiAvg.Caption = "Average FPS: " + stats.AvgFPS;
-        mGuiCurr.Caption = "Current FPS: " + stats.LastFPS;
-        mGuiBest.Caption = "Best FPS: " + stats.BestFPS + " " + stats.BestFrameTime + " ms";
-        mGuiWorst.Caption = "Worst FPS: " + stats.WorstFPS + " " + stats.WorstFrameTime + " ms";
-        mGuiTris.Caption = "Triangle Count: " + stats.TriangleCount;
-        mModesText.Caption = mAdditionalInfo;
-
-        timeSinceLastDebugUpdate = 0;
-      } else {
-        timeSinceLastDebugUpdate += timeFragment;
-      }
-    }
-
-    protected RenderWindow mWindow;
-    protected float timeSinceLastDebugUpdate = 1;
-    protected OverlayElement mGuiAvg;
-    protected OverlayElement mGuiCurr;
-    protected OverlayElement mGuiBest;
-    protected OverlayElement mGuiWorst;
-    protected OverlayElement mGuiTris;
-    protected OverlayElement mModesText;
-    protected string mAdditionalInfo = "";
-
-  }
 
 }
---
+++ MSpriteRenderer/Source/TextManager.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mogre;
+
+namespace MSpriteRenderer
+{
+    public class TextManager
+    {
+        OverlayContainer _OverlayPanel;
+        Overlay _TextOverlay;
+        private uint textCounter;
+
+        public TextManager()
+        {
+            this.Initalize();
+        }
+
+        private void Initalize()
+        {
+            // Create the font resources 
+            // add the path to your resources.cfg
+            // for example:
+            // Default   | ../../../Media/fonts     | ../../../Media/fonts.zip
+            Load("LiberationSans-Regular.ttf", Font.Default, 10);
+            Load("LiberationSans-Bold.ttf", Font.DefaultBold, 15);
+            Load("LiberationSans-Italic.ttf", Font.DefaultItalic, 10);
+            //Load("LiberationMono-Regular.ttf", Font.Console, 26);
+
+            // Create the overlay panel
+            _OverlayPanel = OverlayManager.Singleton.CreateOverlayElement("Panel", new Guid().ToString()) as OverlayContainer;
+            _OverlayPanel.MetricsMode = GuiMetricsMode.GMM_PIXELS;
+            _OverlayPanel.SetPosition(0, 0);
+            _OverlayPanel.SetDimensions(300, 120);
+
+            _TextOverlay = OverlayManager.Singleton.Create(new Guid().ToString());
+
+            _TextOverlay.Add2D(_OverlayPanel);
+        }
+
+        private void Load(string fontFileName, string fontRef, int size)
+        {
+            ResourcePtr font = FontManager.Singleton.Create(fontRef, ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME);
+            font.SetParameter("type", "truetype");
+            font.SetParameter("source", fontFileName);
+            font.SetParameter("size", size.ToString());
+            font.SetParameter("resolution", "96");
+            font.Load();
+        }
+
+        public void HideAllText()
+        {
+            _TextOverlay.Hide();
+        }
+        public void ShowAllText()
+        {
+            _TextOverlay.Show();
+        }
+
+        //returns a texthandle
+        public string AddText(Text text)
+        {
+            _OverlayPanel.AddChild(text._TextArea);
+            return text._TextArea.Name;
+        }
+        public void RemoveText(Text text)
+        {
+            _OverlayPanel.RemoveChild(text._TextArea.Name);
+            OverlayManager.Singleton.DestroyOverlayElement(text._TextArea.Name);
+        }
+        public void RemoveText(string textAreaName)
+        {
+            _OverlayPanel.RemoveChild(textAreaName);
+            OverlayManager.Singleton.DestroyOverlayElement(textAreaName);
+        }
+    }
+
+    public class Text
+    {
+        public TextAreaOverlayElement _TextArea;
+        public string TextName = Guid.NewGuid().ToString();
+
+        public Text(string caption)
+        {
+            _TextArea = OverlayManager.Singleton.CreateOverlayElement("TextArea", TextName) as TextAreaOverlayElement;
+            _TextArea.MetricsMode = GuiMetricsMode.GMM_PIXELS;
+            _TextArea.SetPosition(0, 0);
+            _TextArea.SetDimensions(300, 120);
+            _TextArea.CharHeight = 20;
+            _TextArea.FontName = Font.Default;
+            _TextArea.Caption = caption;
+            _TextArea.Colour = ColourValue.White;
+        }
+
+        public void Hide()
+        {
+            _TextArea.Hide();
+        }
+        public void Show()
+        {
+            _TextArea.Show();
+        }
+
+        public System.Drawing.Point Position
+        {
+            set { _TextArea.SetPosition(value.X, value.Y); }
+            get { return new System.Drawing.Point((int)_TextArea._getLeft(), (int)_TextArea._getTop()); }
+        }
+        public string FontName
+        {
+            set { _TextArea.FontName = value; }
+            get { return _TextArea.FontName; }
+        }
+        public float CharacterHeight
+        {
+            set { _TextArea.CharHeight = value; }
+            get { return _TextArea.CharHeight; }
+        }
+
+        public string Caption
+        {
+            set { _TextArea.Caption = value; }
+            get { return _TextArea.Caption; }
+        }
+
+        public ColourValue Color
+        {
+            set { _TextArea.Colour = value; }
+            get { return _TextArea.Colour; }
+        }
+        public ColourValue ColorTop
+        {
+            set { _TextArea.ColourTop = value; }
+            get { return _TextArea.ColourTop; }
+        }
+        public ColourValue ColorBottom
+        {
+            set { _TextArea.ColourBottom = value; }
+            get { return _TextArea.ColourBottom; }
+        }
+    }
+
+    public class Font
+    {
+        public const string Default = "FONT.DEFAULT";
+        public const string DefaultBold = "FONT.DEFAULT.BOLD";
+        public const string DefaultItalic = "FONT.DEFAULT.ITALIC";
+        public const string Console = "FONT.CONSOLE";
+    }
+}
